070557d502381bd59e5b55a70a625db13e4408da
kimpors <mikita_korshak@ukr.net>
2024-12-08 19:38:16 +0200

    Add testing
    add tests for arg parse
    add tests for expression evaluation

---
+++ test/test_arg.c
@@ -0,0 +1,6 @@
+#include "arg.h"
+
+int main(int argc, char *argv[])
+{
+	return arg_evaluate(argc, argv);
+}
---
+++ test/test_eval.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+#include "parse.h"
+
+#define EPSILON 1e-2
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("wrong args\n");
+		return -1;
+	}
+
+	tokenize(argv[1], TOKEN);
+	double res = evaluate(parse());
+	double arg = atof(argv[2]);
+
+	if (isinf(res))
+	{
+		return !(res == arg);
+	}
+
+	return !(fabs(arg - res) < EPSILON);
+}
