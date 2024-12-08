1d749887a3363b742f57bac3a95b7dbecaa9a4d0
kimpors <mikita_korshak@ukr.net>
2024-12-08 20:13:35 +0200

    Add tests for format evaluation

---
+++ test/test_format.c
@@ -0,0 +1,20 @@
+#include "arg.h"
+#include "token.h"
+#include "format.h"
+#include "parse.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	if (arg_evaluate(argc - 1, argv)) return -1;
+	tokenize(arg_text, TOKEN);
+	char *arg = argv[argc - 1];
+	char *eval = format_eval(evaluate(parse()));
+
+	printf("arg: %s\n", arg);
+	printf("eval: %s\n", eval);
+	printf("cmp: %d\n", strcmp(eval, "-> 2\n"));
+
+	return strcmp(arg, eval);
+}
